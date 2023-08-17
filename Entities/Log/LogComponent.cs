921fe9e2ae77e160b538bb2cf0697f494ed4e916
Leandro Baena Torres <leandrobaena@paradigma.com.co>
2023-08-17 17:15:01 -0500

    Entidades unificadas

---
+++ Entities/Log/LogComponent.cs
@@ -0,0 +1,49 @@
+﻿namespace Entities.Log
+{
+    /// <summary>
+    /// Registra una acción sobre un componente del sistema (I-Información o E-Error)
+    /// </summary>
+    public class LogComponent : EntityBase
+    {
+        #region Attributes
+        /// <summary>
+        /// Fecha del registro
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        /// Tipo de registro
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Componente sobre el que se realizó la acción
+        /// </summary>
+        public string Component { get; set; }
+
+        /// <summary>
+        /// Descripción de lo que se realizó
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Identificador del usuario que realizó la acción
+        /// </summary>
+        public int User { get; set; }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Crea un registro de acción sobre la base de datos con valores por defecto
+        /// </summary>
+        public LogComponent() : base()
+        {
+            Date = DateTime.Now;
+            Type = string.Empty;
+            Component = string.Empty;
+            Description = string.Empty;
+            User = 0;
+        }
+        #endregion
+    }
+}
